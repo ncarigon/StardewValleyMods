5803db0172b814e2e884803bccfc5a2e57c807ea
ncarigon <ncarigon@outlook.com>
2025-02-03 21:07:21 -0500

    Verify base game events still exist before attempting to modify them.

--- GardenPotOptions/Events.cs
+++ GardenPotOptions/Events.cs
@@ -19,19 +19,22 @@ namespace GardenPotOptions {
             } else if (e.NameWithoutLocale.Name.Equals("Data/Events/Farm", StringComparison.InvariantCultureIgnoreCase)) {
                 e.Edit(a => {
                     var d = a.AsDictionary<string, string>().Data;
-                    var hp = ModEntry.Instance?.ModConfig?.HeartsForGardenPot ?? -1;
-                    var hr = ModEntry.Instance?.ModConfig?.HeartsForRecipe ?? -1;
-                    if (hp > -1) {
-                        d[$"{ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/f Evelyn {hp * 250}/k 900553/sendmail {ModEntry.Instance?.ModHarmony?.Id}_Pot"] = "null";
-                        if (hr >= hp) {
-                            d[$"{ModEntry.Instance?.ModHarmony?.Id}_Recipe_Send/f Evelyn {hr * 250}/k 900553/e {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/sendmail {ModEntry.Instance?.ModHarmony?.Id}_Recipe"] = "null";
+                    // make sure the base game event exists before we try to modify it
+                    if (d.ContainsKey("900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny")) {
+                        var hp = ModEntry.Instance?.ModConfig?.HeartsForGardenPot ?? -1;
+                        var hr = ModEntry.Instance?.ModConfig?.HeartsForRecipe ?? -1;
+                        if (hp > -1) {
+                            d[$"{ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/f Evelyn {hp * 250}/k 900553/sendmail {ModEntry.Instance?.ModHarmony?.Id}_Pot"] = "null";
+                            if (hr >= hp) {
+                                d[$"{ModEntry.Instance?.ModHarmony?.Id}_Recipe_Send/f Evelyn {hr * 250}/k 900553/e {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/sendmail {ModEntry.Instance?.ModHarmony?.Id}_Recipe"] = "null";
+                            }
+                            // add new default with requirement
+                            d[$"900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny/k {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send"] = d["900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny"];
+                            // add only recipe event
+                            d[$"900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny/e {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/k {ModEntry.Instance?.ModHarmony?.Id}_Recipe_Send"] = ModEntry.Instance?.Helper.Translation.Get($"{ModEntry.Instance?.ModHarmony?.Id}/event_text_recipe") ?? "null";
+                            // remove default
+                            d.Remove("900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny");
                         }
-                        // add new default with requirement
-                        d[$"900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny/k {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send"] = d["900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny"];
-                        // add only recipe event
-                        d[$"900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny/e {ModEntry.Instance?.ModHarmony?.Id}_Pot_Send/k {ModEntry.Instance?.ModHarmony?.Id}_Recipe_Send"] = ModEntry.Instance?.Helper.Translation.Get($"{ModEntry.Instance?.ModHarmony?.Id}/event_text_recipe") ?? "null";
-                        // remove default
-                        d.Remove("900553/t 600 1130/Hn ccPantry/A cc_Greenhouse/w sunny");
                     }
                 });
             //} else if (e.NameWithoutLocale.Name.Equals("Strings/Objects", StringComparison.InvariantCultureIgnoreCase)) {
