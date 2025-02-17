ef4007b4f342187bf8f0baa73a10ebfe04a31acd
ncarigon <ncarigon@outlook.com>
2025-02-17 08:59:10 -0500

    Added support to water slime hutch troughs and pet bowls.
    Corrected config bug.
    
    Split code into separate files for maintainability.

--- AdjustableSprinklers/Config.cs
+++ AdjustableSprinklers/Config.cs
@@ -2,6 +2,8 @@ using StardewModdingAPI;
 
 namespace AdjustableSprinklers {
     public class Config {
+        public static Config Instance { get; private set; } = new();
+
         public int BaseRadius { get; set; } = 1;
         public int TierIncrease { get; set; } = 1;
         public bool CircularArea { get; set; } = true;
@@ -9,77 +11,88 @@ namespace AdjustableSprinklers {
         public bool ShowScarecrowArea { get; set; } = true;
         public bool ActivateWhenClicked { get; set; } = true;
         public bool WaterGardenPots { get; set; } = true;
+        public bool WaterPetBowls { get; set; } = true;
+        public bool WaterSlimeHutch { get; set; } = true;
 
-        internal static Config Register(IModHelper helper) {
-            var config = helper.ReadConfig<Config>();
-
-            helper.Events.GameLoop.GameLaunched += (s, e) => {
-                var configMenu = helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        internal static void Register(ModEntry mod) {
+            Instance = mod.Helper.ReadConfig<Config>();
+            mod.Helper.Events.GameLoop.GameLaunched += (s, e) => {
+                var configMenu = mod.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
                 if (configMenu is null)
                     return;
-
-                var manifest = helper.ModRegistry.Get(helper.ModContent.ModID)!.Manifest;
                 configMenu.Register(
-                    mod: manifest,
-                    reset: () => config = new Config(),
-                    save: () => helper.WriteConfig(config)
+                    mod: mod.ModManifest,
+                    reset: () => Instance = new Config(),
+                    save: () => mod.Helper.WriteConfig(Instance)
                 );
                 configMenu.AddNumberOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Base Radius",
-                    tooltip: () => "Set the starting radius for sprinkler watering. 0 = Default Game Value",
-                    getValue: () => config.BaseRadius,
-                    setValue: value => config.BaseRadius = value,
+                    tooltip: () => "Set the starting radius for sprinkler watering. Default Game Value: 1",
+                    getValue: () => Instance.BaseRadius,
+                    setValue: value => Instance.BaseRadius = value,
                     min: 0,
                     max: 10
                 );
                 configMenu.AddNumberOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Tier Increase",
-                    tooltip: () => "Set the radius increase each sprinkler upgrade applies. 1 = Default Game Value",
-                    getValue: () => config.BaseRadius,
-                    setValue: value => config.BaseRadius = value,
+                    tooltip: () => "Set the radius increase each sprinkler upgrade applies. Default Game Value: 1",
+                    getValue: () => Instance.TierIncrease,
+                    setValue: value => Instance.TierIncrease = value,
                     min: 1,
                     max: 10
                 );
                 configMenu.AddBoolOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Circular Sprinkler Area",
                     tooltip: () => "Watered tiles by a sprinkler use a circular area instead of square.",
-                    getValue: () => config.CircularArea,
-                    setValue: value => config.CircularArea = value
+                    getValue: () => Instance.CircularArea,
+                    setValue: value => Instance.CircularArea = value
                 );
                 configMenu.AddBoolOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Show Sprinkler Area",
                     tooltip: () => "Shows tiles to be watered by a sprinkler on placement.",
-                    getValue: () => config.ShowSprinklerArea,
-                    setValue: value => config.ShowSprinklerArea = value
+                    getValue: () => Instance.ShowSprinklerArea,
+                    setValue: value => Instance.ShowSprinklerArea = value
                 );
                 configMenu.AddBoolOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Show Scarecrow Area",
                     tooltip: () => "Shows tiles to be protected by a scarecrow on placement.",
-                    getValue: () => config.ShowScarecrowArea,
-                    setValue: value => config.ShowScarecrowArea = value
+                    getValue: () => Instance.ShowScarecrowArea,
+                    setValue: value => Instance.ShowScarecrowArea = value
                 );
                 configMenu.AddBoolOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Activate When Clicked",
                     tooltip: () => "Activates a sprinkler when you click on it.",
-                    getValue: () => config.ActivateWhenClicked,
-                    setValue: value => config.ActivateWhenClicked = value
+                    getValue: () => Instance.ActivateWhenClicked,
+                    setValue: value => Instance.ActivateWhenClicked = value
                 );
                 configMenu.AddBoolOption(
-                    mod: manifest,
+                    mod: mod.ModManifest,
                     name: () => "Water Garden Pots",
                     tooltip: () => "Allow sprinklers to water garden pots.",
-                    getValue: () => config.WaterGardenPots,
-                    setValue: value => config.WaterGardenPots = value
+                    getValue: () => Instance.WaterGardenPots,
+                    setValue: value => Instance.WaterGardenPots = value
+                );
+                configMenu.AddBoolOption(
+                    mod: mod.ModManifest,
+                    name: () => "Water Pet Bowls",
+                    tooltip: () => "Allow sprinklers to water pet bowls.",
+                    getValue: () => Instance.WaterPetBowls,
+                    setValue: value => Instance.WaterPetBowls = value
+                );
+                configMenu.AddBoolOption(
+                    mod: mod.ModManifest,
+                    name: () => "Water Slime Hutch Troughs",
+                    tooltip: () => "Allow sprinklers to water slime hutch troughs.",
+                    getValue: () => Instance.WaterSlimeHutch,
+                    setValue: value => Instance.WaterSlimeHutch = value
                 );
             };
-
-            return config;
         }
     }
 
---
+++ AdjustableSprinklers/Patches.cs
@@ -0,0 +1,163 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Objects;
+using StardewValley.Tools;
+using SObject = StardewValley.Object;
+
+namespace AdjustableSprinklers {
+    internal static class Patches {
+        public static void Register(ModEntry mod) {
+            var harmony = new Harmony(mod.ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SObject), "GetBaseRadiusForSprinkler"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Postfix_Object_GetBaseRadiusForSprinkler))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SObject), "GetModifiedRadiusForSprinkler"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Postfix_Object_GetModifiedRadiusForSprinkler))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SObject), "GetSprinklerTiles"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Postfix_Object_GetSprinklerTiles))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SObject), "drawPlacementBounds"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Postfix_Object_drawPlacementBounds))
+            );
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SObject), "ApplySprinkler"),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Postfix_Object_ApplySprinkler))
+            );
+            mod.Helper.Events.Input.ButtonPressed += Input_ButtonPressed;
+        }
+
+        private static readonly Random R = new();
+
+        [HarmonyPriority(Priority.VeryLow)]
+        private static void Postfix_Object_GetBaseRadiusForSprinkler(
+            SObject __instance, ref int __result
+        ) {
+            if (__instance is not null && __result != -1) {
+                var radius = Config.Instance.BaseRadius;
+                var increase = Config.Instance.TierIncrease;
+                __result = __instance.QualifiedItemId switch {
+                    "(O)599" => radius,
+                    "(O)621" => radius + increase,
+                    "(O)645" => radius + (increase * 2),
+                    "(O)1113" => radius + (increase * 3),
+                    _ => -1
+                };
+                if (__result == -1) {
+                    __result = __instance.Name switch {
+                        "Sprinkler" => radius,
+                        "Quality Sprinkler" => radius + increase,
+                        "Iridium Sprinkler" => radius + (increase * 2),
+                        "Prismatic Sprinkler" => radius + (increase * 3),
+                        _ => -1
+                    };
+                }
+            }
+        }
+
+        [HarmonyPriority(Priority.VeryLow)]
+        private static void Postfix_Object_GetModifiedRadiusForSprinkler(
+            SObject __instance, ref int __result
+        ) {
+            if (__instance is not null && __result != -1) {
+                if (__instance.heldObject.Value != null && __instance.heldObject.Value.QualifiedItemId == "(O)915") {
+                    // need to -1 here to remove vanilla game logic and adjust to the configured value
+                    __result += Config.Instance.TierIncrease - 1;
+                }
+            }
+        }
+
+        [HarmonyPriority(Priority.VeryLow)]
+        private static void Postfix_Object_GetSprinklerTiles(
+            SObject __instance, ref List<Vector2> __result
+        ) {
+            if (Config.Instance.CircularArea == true && __instance?.IsSprinkler() == true) {
+                __result = GetTiles(__instance.TileLocation, __instance.GetModifiedRadiusForSprinkler(), true);
+            }
+        }
+
+        private static void Postfix_Object_drawPlacementBounds(
+            SObject __instance, SpriteBatch spriteBatch
+        ) {
+            if (__instance is not null && spriteBatch is not null
+                && (Config.Instance.ShowSprinklerArea == true || Config.Instance.ShowScarecrowArea == true)
+            ) {
+                List<Vector2>? tiles = null;
+                if (Config.Instance.ShowSprinklerArea == true && __instance.IsSprinkler()) {
+                    tiles = __instance.GetSprinklerTiles();
+                } else if (Config.Instance.ShowScarecrowArea == true && __instance.IsScarecrow()) {
+                    tiles = GetTiles(__instance.TileLocation, __instance.GetRadiusForScarecrow(), false);
+                }
+                if (tiles is not null) {
+                    foreach (Vector2 v in tiles) {
+                        spriteBatch.Draw(Game1.mouseCursors, Game1.GlobalToLocal(new Vector2((int)v.X * 64, (int)v.Y * 64)), new Rectangle(194, 388, 16, 16), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0.01f);
+                    }
+                }
+            }
+        }
+
+        private static List<Vector2> GetTiles(Vector2 center, int radius, bool round) {
+            var tiles = new List<Vector2>();
+            for (var x = -radius; x <= radius; x++) {
+                for (var y = -radius; y <= radius; y++) {
+                    var tile = center + new Vector2(x, y);
+                    if (Vector2.Distance(center, tile) < radius + (round ? 0.5 : 0.0)) {
+                        tiles.Add(tile);
+                    }
+                }
+            }
+            return tiles;
+        }
+
+        private static void Input_ButtonPressed(object? sender, StardewModdingAPI.Events.ButtonPressedEventArgs e) {
+            if (Config.Instance.ActivateWhenClicked == true
+                && e?.Button.IsActionButton() == true
+                && Game1.player?.currentLocation is not null
+            ) {
+                var obj = Game1.player.currentLocation.getObjectAtTile((int)e.Cursor.GrabTile.X, (int)e.Cursor.GrabTile.Y);
+                if (obj?.IsSprinkler() == true) {
+                    obj.ApplySprinklerAnimation();
+                    foreach (var tile in obj.GetSprinklerTiles()) {
+                        Task.Delay(R.Next(500, 2000)) // INFO: adding a delay here for a more natural look
+                            .ContinueWith(_ => obj.ApplySprinkler(tile));
+                    }
+                }
+            }
+        }
+
+        private static void Postfix_Object_ApplySprinkler(
+            SObject __instance, Vector2 tile
+        ) {
+            if (Config.Instance.WaterGardenPots == true
+                && __instance?.Location?.Objects?.TryGetValue(tile, out var t) == true
+                && t is IndoorPot pot && pot is not null
+            ) {
+                Task.Delay(R.Next(500, 2000)) // INFO: adding a delay here for a more natural look
+                            .ContinueWith(_ => pot?.Water());
+            }
+            if (Config.Instance.WaterPetBowls == true) {
+                __instance?.Location?.buildings?
+                    .Where(b => b.occupiesTile(tile) && b is PetBowl)?
+                    .Select(b => b as PetBowl)
+                    .Where(b => b is not null)
+                    .Do(b => Task.Delay(R.Next(500, 2000)) // INFO: adding a delay here for a more natural look
+                            .ContinueWith(_ => b!.watered.Value = true)
+                    );
+            }
+            if (Config.Instance.WaterSlimeHutch == true
+                && __instance?.Location is SlimeHutch hutch
+            ) {
+                Task.Delay(R.Next(500, 2000)) // INFO: adding a delay here for a more natural look
+                            .ContinueWith(_ => hutch.performToolAction(default(WateringCan), (int)tile.X, (int)tile.Y));
+            }
+        }
+    }
+}
